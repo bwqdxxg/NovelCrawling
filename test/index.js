9558c41c8cd5570082d79ee412b3ad9d6499cb1e
bwqdxxg <215943108@qq.com>
2019-11-14 20:22:40 +0800

    1.测试服务器改为express

--- test/index.js
+++ test/index.js
@@ -1,59 +1,33 @@
-var http = require('http');
-// url 做路径解析
-var url = require('url');
-// fs 读写文件
-var fs = require('fs');
-// node 服务
-var server = http.createServer();
-// 获取html相对路径
-var htmlDir = __dirname + '/';
+var express = require('express');
+var app = express();
 
-// 处理url请求的数据
-function sendData(file, req, res) {
-    fs.readFile(file, function (err, data) {
-        if (err) {
-            res.writeHead(404, {
-                'content-type': 'text/html'
-            });
-            res.write('<h1>404<h1/>');
-            res.end();
-        } else {
-            res.writeHead(200, {
-                'content-type': 'text/html'
-            });
-            res.write(data);
-            res.end();
-        }
-    });
-}
-// 监听服务开启
-server.on('listening', function () {
-    console.log('listen..');
+// 设置跨域访问
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+    res.header("X-Powered-By", ' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8")
+    next()
+})
+
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/" + "index.html");
 });
-server.on('request', function (req, res) {
-    // 获取url后面的路径
-    var urlStr = url.parse(req.url);
-    console.log(urlStr.pathname);
-    switch (urlStr.pathname) {
-        case '/':
-            // 首页
-            sendData(htmlDir + 'index.html', req, res);
-            break;
-        case '/bookList':
-            // 首页
-            sendData(htmlDir + 'bookList.html', req, res);
-            break;
-        case '/book':
-            // 首页
-            sendData(htmlDir + 'book.html', req, res);
-            break;
-        case '/content':
-            // 首页
-            sendData(htmlDir + 'content.html', req, res);
-            break;
-        default:
-            sendData(htmlDir + 'error.html', req, res);
-            break;
-    }
+
+app.get('/bookList', function (req, res) {
+    res.sendFile(__dirname + "/" + "bookList.html");
 });
-server.listen(3000, 'localhost');
+
+app.get('/book', function (req, res) {
+    res.sendFile(__dirname + "/" + "book.html");
+});
+
+app.get('/content', function (req, res) {
+    res.sendFile(__dirname + "/" + "content.html");
+});
+
+var server = app.listen(3000)
